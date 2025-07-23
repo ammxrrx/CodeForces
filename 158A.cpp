388c042774be4680e3ec8a7d5dea9921d49339de
ammxrrx <ammar1great@gmail.com>
2025-07-23 18:20:16 +0500

    Solved

---
+++ 158A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, k;
+    int c=0;
+    cin>>n>>k;
+    int *arr;
+    arr=new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]>=arr[k-1]&&arr[i]!=0)
+        {
+                c++;
+        }
+    }
+    cout<<c<<endl;
+    return 0;
+}
